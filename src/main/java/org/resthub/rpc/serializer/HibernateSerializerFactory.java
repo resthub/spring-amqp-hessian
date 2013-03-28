9a07450dc51d543f2a777cb4d2137e36913a93a4
JRI <julien.ripault@atos.net>
2013-03-28 21:19:40 +0100

    manage map mapping

--- src/main/java/org/resthub/rpc/serializer/HibernateSerializerFactory.java
+++ src/main/java/org/resthub/rpc/serializer/HibernateSerializerFactory.java
@@ -3,24 +3,27 @@ package org.resthub.rpc.serializer;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.caucho.hessian.io.*;
 import org.hibernate.Hibernate;
 import org.hibernate.collection.internal.AbstractPersistentCollection;
-
-import com.caucho.hessian.io.AbstractHessianOutput;
-import com.caucho.hessian.io.CollectionSerializer;
-import com.caucho.hessian.io.HessianProtocolException;
-import com.caucho.hessian.io.Serializer;
-import com.caucho.hessian.io.SerializerFactory;
+import org.hibernate.collection.internal.PersistentMap;
 
 public class HibernateSerializerFactory extends SerializerFactory {
 	
 	private HibernateListSerializer listSerializer = new HibernateListSerializer();
+	private HibernateMapSerializer mapSerializer = new HibernateMapSerializer();
 
 	@SuppressWarnings("rawtypes")
 	public Serializer getSerializer(Class cl) throws HessianProtocolException {
-		return (Serializer) (AbstractPersistentCollection.class
-				.isAssignableFrom(cl) ? listSerializer : super.getSerializer(cl));
+       if(PersistentMap.class.isAssignableFrom(cl)){
+            return mapSerializer;
+       }else if (AbstractPersistentCollection.class.isAssignableFrom(cl)){
+            return listSerializer;
+       }
+		return super.getSerializer(cl);
 	}
 	
 	private static class HibernateListSerializer implements Serializer {
@@ -38,5 +41,22 @@ public class HibernateSerializerFactory extends SerializerFactory {
 			}
 		}
 		
+	}
+
+	private static class HibernateMapSerializer implements Serializer {
+
+		private MapSerializer delegate = new MapSerializer();
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public void writeObject(Object obj, AbstractHessianOutput out)
+				throws IOException {
+			if (Hibernate.isInitialized(obj)){
+				delegate.writeObject(new HashMap((Map) obj), out);
+			}
+			else {
+				delegate.writeObject(new HashMap(), out);
+			}
+		}
+
 	}
 }
